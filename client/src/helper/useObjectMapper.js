a0682605eeb9fb31eb10b57a483ce1765015f110
Kaisu Karvonen <kaisu.karvonen@robu.fi>
2022-05-31 22:47:20 +0300

    use optional chaining

--- client/src/helper/useObjectMapper.js
+++ client/src/helper/useObjectMapper.js
@@ -2,12 +2,12 @@ import { useSelector } from 'react-redux';
 
 export const useObjectMapper = () => {
   const fields = useSelector((state) => state.fields.fields);
-  
+
   const objectValue = (key) => fields.find((field) => field.key === key);
 
   const translation = (key) => {
     const value = objectValue(key);
-    return value ? value.fi || key : key;
+    return value?.fi || key;
   };
 
   const getObjectInList = (key, innerKey) =>

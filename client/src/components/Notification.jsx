ed2d9ec3f54e2b2a465a85b072d8041e8b11190d
kaisukarvonen <kaisu.karvonen@smartum.fi>
2023-03-07 08:54:00 +0200

    edit email sent text

--- client/src/components/Notification.jsx
+++ client/src/components/Notification.jsx
@@ -4,17 +4,25 @@ import { Modal, Icon, Transition } from 'semantic-ui-react';
 const Notification = ({ hideNotification, notification }) => {
   const [visible, setVisibility] = useState(true);
 
-  const successMsg =
-    'Kiitokset yhteydenotostasi, pyrimme vastaamaan sinulle vuorokauden sisällä maanantaista perjantaihin. Olemme lähettäneet sinulle kopion tarjouspyynnöstä sähköpostiisi.';
   const errorMsg = 'Viestiäsi ei valitettavasti voitu lähettää, yritä myöhemmin uudelleen';
   return (
     <Transition visible={visible} unmountOnHide transitionOnMount onHide={hideNotification}>
       <Modal dimmer="inverted" size="small" open>
         <Modal.Content>
           <Icon link name="close" style={{ float: 'right' }} onClick={() => setVisibility(false)} />
-          <p style={{ fontSize: '16px' }}>
+          <p style={{ fontSize: '16px', textAlign: 'center' }}>
             {notification.success ? (
-              successMsg
+              <>
+                <span>Kiitos, kun otit yhteyttä!</span>
+                <br />
+                <span>
+                  Malta vielä tovi. Pyrimme aina vastaamaan vuorokauden sisällä maanantaista
+                  perjantaihin. Saat sähköpostiin kopion laittamastasi tarjouspyynnöstä.
+                </span>
+                <br />
+                <br />
+                <span>Terveisin, Nuuksion Taika Tiimi</span>
+              </>
             ) : (
               <>
                 <Icon name="exclamation triangle" /> {errorMsg}
